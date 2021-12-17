e5d02d037ef5aa770c02cbdad1c68ab79c969a21
Roman <63646611+RomanPalna@users.noreply.github.com>
2021-12-17 16:21:38 +0200

    section trusted person add

--- src/components/Loan/Information/Information.js
+++ src/components/Loan/Information/Information.js
@@ -1,10 +1,12 @@
 import NameInput from "./NameInput";
+import TrusterPeople from "./Trusted";
 
 export default function Information() {
   return (
     <div className="information">
       <p className="information__title">Информация о заёмщике</p>
       <NameInput />
+      <TrusterPeople />
     </div>
   );
 }
