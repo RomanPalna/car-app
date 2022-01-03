8a34b0b9fe3a5aebbc075fbbd4ea48d44508ca30
Roman <63646611+RomanPalna@users.noreply.github.com>
2022-01-03 12:32:27 +0200

    footer

---
+++ src/components/Footer/Copyright.js
@@ -0,0 +1,10 @@
+export default function Copyright() {
+  return (
+    <div>
+      <p>Crystal Motors (c) 2021</p>
+      <p>
+        Разработка сайта <img alt="devlogo" src=""></img>
+      </p>
+    </div>
+  );
+}
