f9dcdbb1b7abb8a86fdd207488b52b5e95581961
Roman Palna <roma.palna@gmail.com>
2022-01-11 17:37:26 +0200

    rendering cars by car cards is done

--- src/components/Loan/Car/CarCard.js
+++ src/components/Loan/Car/CarCard.js
@@ -6,15 +6,6 @@ import * as autoOperations from "../../../redux/autosCard/auto-operations";
 import CheckboxLabels from "./CheckboxLabel";
 import noImage from "../../../images/noImage.jpg";
 
-const car = {
-  image: noImage,
-  year: 2021,
-  engine: "автомат",
-  brand: "Hyundai",
-  model: "Solaris",
-  price: 14000,
-};
-
 export default function CarCard() {
   const dispatch = useDispatch();
 
@@ -32,33 +23,45 @@ export default function CarCard() {
 
   return (
     <>
-      <p className="car__count">Найдено 1 автомобиль</p>
+      {carCards.length === 0 ? (
+        <p>Не найдено автомобилей отвечающим Вашим параметрам запроса</p>
+      ) : (
+        <p className="car__count">Найдено {carCards.length} автомобиль</p>
+      )}
+      <div className="car__container">
+        {carCards.map((car) => (
+          <div className="car" key={car.userId}>
+            <img
+              className="car__img"
+              alt="car"
+              src={car.photoData.seoLinkF ?? noImage}
+            />
 
-      <div className="car">
-        <img className="car__img" alt="car" src={car.image} />
+            <form className="car__form">
+              <p className="car__form--year">
+                <span>{car.autoData.year} год</span> /{" "}
+                <span>{car.autoData.gearboxName}</span>
+              </p>
+              <p className="car__form--model">
+                {car.markName} {car.modelName} {car.autoData.year} {"года"}
+              </p>
+              <p className="car__form--credit">
+                <span className="car__form--credit--price">{car.UAH} грн</span>
+                <span className="car__form--credit--loan">
+                  от {car.price / 14} грн/месяц
+                </span>
+              </p>
+              <p className="car__form--protect">
+                <span className="car__form--protect--price">{car.UAH} грн</span>
+                <span className="car__form--protect--loan">
+                  от {Number(car.price) / 14} грн/месяц
+                </span>
+              </p>
 
-        <form className="car__form">
-          <p className="car__form--year">
-            <span>{car.year} год</span> / <span>{car.engine}</span>
-          </p>
-          <p className="car__form--model">
-            {car.brand} {car.model} {car.year} {"года"}
-          </p>
-          <p className="car__form--credit">
-            <span className="car__form--credit--price">{car.price} грн</span>
-            <span className="car__form--credit--loan">
-              от {car.price / 14} грн/месяц
-            </span>
-          </p>
-          <p className="car__form--protect">
-            <span className="car__form--protect--price">{car.price} грн</span>
-            <span className="car__form--protect--loan">
-              от {car.price / 14} грн/месяц
-            </span>
-          </p>
-
-          <CheckboxLabels />
-        </form>
+              <CheckboxLabels />
+            </form>
+          </div>
+        ))}
       </div>
     </>
   );
